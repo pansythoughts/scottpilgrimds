0304ba5c98d4cb9f8dfbed95cf8d97a832025251
pansythoughts <alejandritolopez910@gmail.com>
2025-06-30 23:07:44 -0700

    progress more le
    basic movement mechanicas (walking, running, jumping, dodging, etc.) fleshed out :D

--- source/main.cpp
+++ source/main.cpp
@@ -15,7 +15,7 @@ constexpr uint8_t CHAR_SCREEN_SIZE = 190;
 
 enum CHAR_SPRITE_SIZE {CHAR_DEFAULT_WIDTH = 64, CHAR_DEFAULT_LENGTH = 64};
 enum CHAR_SPRITE_ANIMATION_STATES {A_IDLE = 0, A_WALK = 1, A_RUN = 2, A_JUMP = 3};
-enum CHAR_SPRITE_PRIMARY_STATES {P_IDLE = 0, P_WALK = 1, P_RUN = 2};
+enum CHAR_SPRITE_PRIMARY_STATES {P_IDLE = 0, P_WALK = 1, P_RUN = 2, P_DODGING = 3};
 enum CHAR_SPRITE_SECONDARY_STATES {S_NONE = 0, S_JUMP = 1};
 
 
@@ -28,6 +28,9 @@ int key_pressed = 0;
 int key_held = 0;
 int key_current = 0;
 int key_down_repeat = 0;
+int key_up = 0;
+int last_dpad_key = 0;
+int frames = 0;
 
 //Clases//
 
@@ -154,16 +157,22 @@ class Sprite{
 }
     //Actualiza al sprite (posicion, tipo de sprite y paleta.).
     void updateSprite(){
+
 	    //Se guarda el estado de la animacion del frame, para compararlo en futuros frames.
 	    past_anim_status = anim_status;
 
 		if(anim_status >= 0 && anim_status <= CHAR_NUM_SPRITES){
+
 	    //Se actualiza la posicion del sprite.
 		if(anim_status_changed)
 		changeSpriteAndPalette();
-		NF_MoveSprite(screen, id, screen_pos_x, screen_pos_y);//(screen, id, pos x, pos y)
+
+		NF_MoveSprite(screen, id, screen_pos_x, screen_pos_y);
+
 		if(flipped)
 		NF_HflipSprite(screen, id, true);
+		else
+		NF_HflipSprite(screen, id, false);
 }
 }
 };
@@ -175,23 +184,32 @@ class Character{
 	int map_pos_x = 0;
 	int map_pos_y = 0;
 	float vel_x = 0;
-	float jump_vel_x = 0;
 	float vel_y = 0;
 	float jump_vel_y = 0;
 	float acc_x = 0;
 	float acc_y = 0;
+	float jump_acc_y = 0;
 	int jump_height = 0;
 	int primary_status = 0; //0.IDLE,1.WALK,2.RUN.
 	int secondary_status = 0; //0.NONE,1.JUMP.
+	int past_primary_status = 0;
+	int past_secondary_status = 0;
+	bool primary_status_changed = false;
+	bool secondary_status_changed = false;
+	bool can_move = false;
+	bool slowing_down = false;
 	int frames_moving = 0;
 	int frames_jumping = 0;
 	bool jumping = false;
+	bool dodging = false;
 	int jump_start_pos = 0;
 	int jump_final_pos = 0;
-	bool canDTap = false;
-	bool canRun = false;
-	int lastTapTime = 0;    // Guardará el tiempo del último toque
-    int doubleTapThreshold = 15; // Cuántos frames para considerar un doble toque
+	bool can_dtap = false;
+	bool can_run = false;
+	bool can_dodge = false;
+	int last_tap_time_x = 0;  
+	int last_tap_time_y = 0;  
+    int double_tap_threshold = 15;
 	Sprite sprite;
 
 	//Metodos//
@@ -200,19 +218,153 @@ class Character{
 	void mapToScreenPos(){
 		//luego hare una version mas aca que permita el scrolleo :D
 		sprite.screen_pos_x = map_pos_x;
-		sprite.screen_pos_y = map_pos_y;
-		
-		if(jumping)
-		sprite.screen_pos_y += jump_height;
+		sprite.screen_pos_y = map_pos_y + jump_height;
+	
 		
 
 	}
 
+	//El personaje realiza la accion de saltar.
+	void Jump(){
+
+		if(!jumping)
+		{
+		sprite.anim_frame = 0;
+		vel_x = 0;
+		jumping = true;
+
+		if(!dodging)
+		jump_vel_y = -6.7;
+		else
+		jump_vel_y = -4.7;
+
+		}
+		
+		if(jumping)
+		{
+		frames_jumping++;
+		}
+
+		if(frames_jumping >= 1 && jumping)
+		{
+		jump_acc_y = 0.3;
+		jump_vel_y += jump_acc_y;
+		jump_height += jump_vel_y;
+	    }
+
+		if(jump_vel_y <= 0 && jump_vel_y >= -0.5 && !dodging)
+		{
+			if(sprite.anim_frame == 1 || sprite.anim_frame == 3)
+			sprite.anim_direction *= -1;
+		}
+		else
+			sprite.anim_direction = 1;
+
+		if(jump_height <= 2 && jump_height >= 6 && frames_jumping > 20)
+		sprite.anim_frame = 12;
+
+	    if(jump_height >= 0)
+		{
+		sprite.anim_frame = 12;
+		frames_jumping = 0;
+		jumping = false;
+		}
+	}
+	
+
 	//Maneja el 'double tap' para correr. 
 	//(luego hacerlo para saltar hacia arriba y abajo).
 	void doubleTap(){
+
+		/*
+		if(key_held & (KEY_LEFT | KEY_RIGHT | KEY_UP | KEY_DOWN))
+		{
+		frames_moving++;
+		}
+
+
+		if(key_up & (KEY_LEFT | KEY_RIGHT | KEY_UP | KEY_DOWN) && frames_moving <= double_tap_threshold)
+		{
+			can_run_or_dodge = can_dtap && (frames_moving - last_tap_time <= double_tap_threshold);
+			can_dtap = true;
+			last_tap_time = frames_moving;
+		}
+
+		if (frames_moving - last_tap_time > double_tap_threshold || jumping) {
+				can_dtap = false;
+		}
+		*/
+
+	
+		if(key_pressed & (KEY_LEFT | KEY_RIGHT | KEY_UP | KEY_DOWN))
+		{
+		//Horizontal (correr).
+		if(key_pressed & (KEY_LEFT | KEY_RIGHT))
+		{
+		if(frames - last_tap_time_x <= double_tap_threshold && !jumping)
+		{
+            can_run = true;
+            can_dtap = false;
+        } 
+		else 
+		{
+            can_run = false;
+            can_dtap = true;
+            last_tap_time_x = frames;
+        }
+	    }
+
+		//Vertical (esquivar).
+		else if(key_pressed & (KEY_UP | KEY_DOWN))
+		{
+		if(frames - last_tap_time_y <= double_tap_threshold && !jumping)
+		{
+            can_dodge = true;
+            can_dtap = false;
+        } 
+		else 
+		{
+            can_dodge = false;
+            can_dtap = true;
+            last_tap_time_y = frames;
+        }
+	    }
+        }
+
+        if(frames - last_tap_time_x > double_tap_threshold && frames - last_tap_time_y > double_tap_threshold )
+        can_dtap = false;
+		
+
+		if(can_run && key_held & (KEY_LEFT | KEY_RIGHT) && last_dpad_key != (KEY_UP | KEY_DOWN))
+		primary_status = P_RUN;
+		else if(can_dodge && key_held & (KEY_UP | KEY_DOWN) && last_dpad_key != (KEY_LEFT | KEY_RIGHT))
+		{
+		dodging = true;
+		vel_y = 0;
+		can_dodge = false;
+		can_run = false;
+		Jump();
+	    }
+		else if(!can_run && key_held & (KEY_LEFT | KEY_RIGHT | KEY_UP | KEY_DOWN) && !jumping)
+		{
+			primary_status = P_WALK;
+		}
+
+		if(jumping && dodging)
+		{
+		slowing_down = false;
+		primary_status = P_DODGING;
+		}
+		else
+		dodging = false;
+
+
+
+
+
+		/*
 		// Revisa si el jugador está presionando las teclas de movimiento
-		if (keysHeld() == KEY_LEFT || keysHeld() == KEY_RIGHT) {
+		if (keysHeld() & KEY_LEFT || keysHeld() & KEY_RIGHT) {
 			frames_moving++;  // Incrementa los frames de movimiento
 		
 		canRun = canDTap && (frames_moving - lastTapTime <= doubleTapThreshold);
@@ -235,10 +387,15 @@ class Character{
 		//Si no puede harcer 'double tap' o esta yendo hacia arriba o abajo, camina.
 		if(!canDTap && ((keysHeld() == KEY_UP) || (keysHeld() == KEY_DOWN) || (keysHeld() == KEY_LEFT) ||(keysHeld() == KEY_RIGHT)))
 			primary_status = P_WALK;
+		*/
 	}
 
 	//Maneja lo relacionado al movimiento del personaje.
 	void moveCharacter(){
+	
+	//Se guardan los estados del personaje para compararlo en futuros frames.
+	past_primary_status = primary_status;
+	past_secondary_status = secondary_status;
 
 	mapToScreenPos();
 	
@@ -253,6 +410,8 @@ class Character{
 		sprite.anim_status = A_WALK; break;
 		case P_RUN:
 		sprite.anim_status = A_RUN; break;
+		case P_DODGING:
+		sprite.anim_status = A_JUMP; break;
 	}
     }
 	else
@@ -263,120 +422,194 @@ class Character{
 		    sprite.anim_status = A_JUMP; break;
 		}
 	}
+
     //Maneja todo lo relacionado al doble tap para correr.
 	doubleTap();
+
 	//Se maneja la velocidad del personaje segun sus estados primarios.
 	switch (primary_status)
 	{
 		//Quieto (idle).
 		case P_IDLE:
-		vel_x = 0;
-		vel_y = 0;
+		vel_y = 0; //ya que en el juego original no hay slow down en el eje y :D
+		can_move = false;
 		break;
 		//Caminando (walking).
 		case P_WALK:
+		can_move = true;
+		if(!jumping)
+		{
+		if(key_held & KEY_RIGHT && !(key_held & KEY_LEFT))
 		vel_x = 2;
+		else if (key_held & KEY_LEFT && !(key_held & KEY_RIGHT))
+		vel_x = -2;
+		}
+		
+		else if(jumping && vel_x == 0)
+		{
+			if(last_dpad_key & KEY_LEFT)
+			vel_x = -2;
+			else if (last_dpad_key & KEY_RIGHT)
+			vel_x = 2;
+		}
+
+		if(key_held & KEY_DOWN && !(key_held & KEY_UP))
 		vel_y = 2;
+		else if (key_held & KEY_UP && !(key_held & KEY_DOWN))
+		vel_y = -2;
+		else 
+		vel_y = 0; //ya que en el juego original no hay slow down en el eje y :D
 		 break;
 		//Corriendo (running).
 		case P_RUN:
+		can_move = true;
+		if(!jumping)
+		{
+		if(key_held & KEY_RIGHT && !(key_held & KEY_LEFT))
 		vel_x = 5;
+		else if (key_held & KEY_LEFT && !(key_held & KEY_RIGHT))
+		vel_x = -5;
+		}
+		else if(jumping && vel_x == 0)
+		{
+			if(last_dpad_key & KEY_LEFT)
+			vel_x = -5;
+			else if (last_dpad_key & KEY_RIGHT)
+			vel_x = 5;
+		}
+
+		if(key_held & KEY_DOWN && !(key_held & KEY_UP))
 		vel_y = 2;
+		else if (key_held & KEY_UP && !(key_held & KEY_DOWN))
+		vel_y = -2;
+		else
+		vel_y = 0; //ya que en el juego original no hay slow down en el eje y :D
+
+		break;
+		//Esquivando.
+		case P_DODGING:
+		if(vel_y == 0)
+		{
+		if(last_dpad_key & KEY_UP)
+		vel_y = -1.5;
+		else if (last_dpad_key & KEY_DOWN)
+		vel_y = 1.5;
+		}
 		break;
 	}
 
 	//Se maneja la logica de los estados secundarios.
 	switch (secondary_status)
 	{
+		//Salto del personaje.
 		case S_JUMP:
-		if(!jumping)
-		{
-		sprite.anim_frame = 0;
-		jumping = true;
-		jump_vel_y = -6.7;
-		}
-		
-		if(jumping)
-		frames_jumping++;
 
-		if(frames_jumping >= 1 && jumping)
-		{
-		acc_y = 0.3;
-		jump_vel_y += acc_y;
-		jump_height += jump_vel_y;
-	    }
+		Jump();
 
-		if(jump_vel_y <= 0 && jump_vel_y >= -0.5)
-		{
-			if(sprite.anim_frame == 1 || sprite.anim_frame == 3)
-			sprite.anim_direction *= -1;
-		}
-		else
-		{
-			sprite.anim_direction = 1;
-		}
-
-		if(jump_height <= 2 && jump_height >= 6 && frames_jumping > 20)
-		sprite.anim_frame = 12;
-
-	    if(jump_height >= 0)
-		{
-		sprite.anim_frame = 12;
-		frames_jumping = 0;
-		jumping = false;
-		}
 		break;
-
 	}
+
 	
 	//Entradas de teclas del usuario para mover al personaje.
-	if(key_current & KEY_LEFT){
-		map_pos_x -= vel_x;
-		NF_HflipSprite(0, 0, true);
-		NF_HflipSprite(0, sprite.id, true);
+	//Izquierda.
+	if(key_held & KEY_LEFT && !(key_held & KEY_RIGHT)){
 		sprite.flipped = true;
 	}
-	if(key_current & KEY_RIGHT){
-		map_pos_x += vel_x;
-		NF_HflipSprite(0, 0, false);
-		NF_HflipSprite(0, sprite.id, false);
+	//Derecha.
+	if(key_held & KEY_RIGHT && !(key_held & KEY_LEFT)){
 		sprite.flipped = false;
 	}
 
-	if(key_current & KEY_DOWN){
+	if(!dodging)
+	{
+	//Abajo.
+	if(key_held & KEY_DOWN && !(key_held & KEY_UP)){
 		if(!(key_held & KEY_LEFT || key_held & KEY_RIGHT))
 		primary_status = P_WALK;
-
-		map_pos_y += vel_y;
 	}
-	if(key_current & KEY_UP){
+	//Arriba.
+	if(key_held & KEY_UP && !(key_held & KEY_DOWN)){
 		if(!(key_held & KEY_LEFT || key_held & KEY_RIGHT))
 		primary_status = P_WALK;
-
-		map_pos_y -= vel_y;
 	}
-
-	if(key_current == 0 || (key_held & KEY_LEFT && key_held & KEY_RIGHT) || (key_held & KEY_UP && key_held & KEY_DOWN))
+    }
+    
+	//Quieto.
+	if(((key_current == 0 || (key_held & KEY_LEFT && key_held & KEY_RIGHT) || (key_held & KEY_UP && key_held & KEY_DOWN))) && !jumping)
+	    {
 		primary_status = P_IDLE;
-	
+	    can_move = false;
+	    }
+
+    //Salto.
 	if(key_pressed & KEY_A || jumping)
 		secondary_status = S_JUMP;
 	else
 		secondary_status = S_NONE;
 
+
+
+	//Desaceleracion cuando el personaje deja de moverse.
+
+	//Horizontal.
+	if(((!(key_held & (KEY_LEFT | KEY_RIGHT))) || (key_held & KEY_LEFT && key_held & KEY_RIGHT)) && !jumping)
+	{
+
+	slowing_down = true;
+
+	if (vel_x > 0) 
+	acc_x = -0.2;
+	else if (vel_x < 0) 
+	acc_x = 0.2;
+	else 
+	acc_x = 0;
+	}
+
+	/*INNECESARIO, EN EL JUEGO ORIGINAL NO HAY DESACELERACION EN EL EJE Y :D
+	//Vertical. REPENSAR CONDICION DE ACA, NO SIRVE, BUGEA, CACA
+	if((primary_status_changed) && !(key_held & (KEY_UP | KEY_DOWN)) || (key_held & KEY_UP && key_held & KEY_DOWN))
+	{
+
+	slowing_down = true;
+
+	if (vel_y > 0) 
+	acc_y = -0.5;
+	else if (vel_y < 0) 
+	acc_y = 0.5;
+	else 
+	acc_y = 0;
+	}
+	*/
+
+	if(std::abs(vel_x) <= 0.5f)
+	{
+	slowing_down = false;
+	vel_x = 0;
+	acc_x = 0;
+	}
+	
+
+    //Movimiento del personaje.
+	map_pos_x += vel_x;
+	map_pos_y += vel_y;
+	vel_x += acc_x;
+	vel_y += acc_y;
 	
     
 	//Determina si hubo un cambio en el estado o 'status'.
-	sprite.anim_status_changed = (sprite.past_anim_status != sprite.anim_status);
+	sprite.anim_status_changed = (sprite.past_anim_status != sprite.anim_status) ;
+	primary_status_changed = (past_primary_status != primary_status);
+	secondary_status_changed = (past_secondary_status != secondary_status);
 
 	//Mecanica que permite al personaje moverse entre pantallas.
 	if(second_screen_enabled)
 	{
 	if (map_pos_y >= CHAR_SCREEN_SIZE || sprite.screen_pos_y <= -30)
-	 sprite.changeScreen();
+	sprite.changeScreen();
 	}
-	
 	}
+
+
 	//Constructor
 	Character(int initial_pos_x, int initial_pos_y){
 		map_pos_x = initial_pos_x;
@@ -432,7 +665,7 @@ int main() {
 	kim.sprite.n_frames[A_JUMP] = 12;
 	kim.sprite.anim_frame_period[A_IDLE] = 8;
 	kim.sprite.anim_frame_period[A_WALK] = 8;
-	kim.sprite.anim_frame_period[A_RUN] = 5;
+	kim.sprite.anim_frame_period[A_RUN] = 4;
 	kim.sprite.anim_frame_period[A_JUMP] = 6;
 	kim.sprite.sprite_name = "kim";
 	kim.sprite.screen = 0;
@@ -444,7 +677,7 @@ int main() {
 
 	
 	//texto pitero :P
-	std::cout << "LO QUE ME COSTO ESTE PEDAZO DE \nMIERDA NOMAMES CHINGUEN TODOS A \nSU MADRE\n\n";
+	std::cout << "ESTO SE VA A BORRAR NOMAS EMPIECE EL BUCLE AAAAAAAAAAA";
 
 
 	//Bucle principal
@@ -454,13 +687,19 @@ int main() {
 		//OJO: NO LLAMAR ACA A NF_CreateSprite(), PUES HARA QUE LA ANIMACION SE ENTRECORTE.
 		//LLAMARLA UNA SOLA VEZ ANTES DEL LOOP.
 
+		//Se cuentan los frames.
+		frames++;
 
 
 		//Se registran los inputs del usuario.
 		scanKeys();
+		if(keysDown() & (KEY_UP | KEY_DOWN | KEY_LEFT | KEY_RIGHT))
+		last_dpad_key = keysDown();
+
 		key_pressed = keysDown();
 		key_held = keysHeld();
 		key_current = keysCurrent();
+		key_up = keysUp();
 
 		//Metodos para actualizar el sprite cada frame.
 		if(kim.sprite.anim_status_changed){
@@ -474,30 +713,23 @@ int main() {
 
 
 		//CONSOLE DEBUG//
-		if(keysCurrent() == KEY_B)
-		{
-		consoleClear();
-		std::cout << "Vram libre: " << NF_TEXVRAM.free;
-		}
-		if(kim.sprite.anim_status_changed)
-		std::cout << "\nCAMBIO";
-
-		if(kim.jumping || keysCurrent() == KEY_A)
-		{
-		consoleClear();
-		std::cout << "FRAMES SALTANDO: " << kim.frames_jumping;
-		}
 		
 		consoleClear();
-		if(kim.sprite.anim_status_changed)
-		std::cout << "\n\nCAMBIO";
 		
 		std::cout << "\nSTART POS: " << kim.jump_start_pos
 		          << "\nCURRENT SCREEN POS: " << kim.sprite.screen_pos_y
 				  << "\nCURRENT MAP POS: " << kim.map_pos_y
 				  << "\nSTATUS PRINCIPAL: " << kim.primary_status
 				  << "\nSTATUS SECUNDARIO: " << kim.secondary_status
-				  << "\nFRAMES SALTANDO: " << kim.frames_jumping;
+				  << "\nFRAMES SALTANDO: " << kim.frames_jumping
+				  << "\nCAMBIO ESTADO PRIMARIO: " << kim.primary_status_changed
+				  << "\nALENTANDOSE: " << kim.slowing_down
+				  << "\nCAMBIO ANIMACION: " << kim.sprite.anim_status_changed
+				  << "\nPUEDE MOVERSE: " << kim.can_move
+				  << "\nPUEDE CORRER: " << kim.can_run
+				  << "\nPUEDE ESQUIVAR: " << kim.can_dodge
+				  << "\nESQUIVANDO: " << kim.dodging
+				  << "\nSALTANDO: " << kim.jumping;
 		
 		if(second_screen_enabled)
 		{
